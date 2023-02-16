e24f1fb3eb8660e36f32fb89b91f9080e9434807
DESKTOP-MK268CN\Mel <lavienvert3@gmail.com>
2023-02-16 12:22:08 +0100

    refac rename patientList

--- app/src/main/java/benavente/melanie/practicapacientes/ui/PatientViewModel.java
+++ app/src/main/java/benavente/melanie/practicapacientes/ui/PatientViewModel.java
@@ -10,7 +10,7 @@ import benavente.melanie.practicapacientes.domain.Patient;
 
 public class PatientViewModel extends ViewModel {
 
-    private MutableLiveData<List<Patient>> pacienteList = new MutableLiveData<>();
+    private MutableLiveData<List<Patient>> patientList = new MutableLiveData<>();
     private MutableLiveData<Patient> actualPatient = new MutableLiveData<>();
 
     public PatientViewModel(){
@@ -26,23 +26,23 @@ public class PatientViewModel extends ViewModel {
         mockList.add(new Patient("Alba", 71, true, 5));
         mockList.add(new Patient("Tona", 17, false,6));
         mockList.add(new Patient("Lali", 22, false,7));
-        pacienteList.setValue(mockList);
+        patientList.setValue(mockList);
     }
 
     public MutableLiveData<List<Patient>> getpatientList(){
-        return pacienteList;
+        return patientList;
     }
 
     public void addPatient(Patient patient){
-        List<Patient> temporaryList = pacienteList.getValue();
+        List<Patient> temporaryList = patientList.getValue();
         temporaryList.add(patient);
-        pacienteList.setValue(temporaryList);
+        patientList.setValue(temporaryList);
     }
 
     public void deletePatient(Patient patient){
-        List<Patient> temporaryList = pacienteList.getValue();
+        List<Patient> temporaryList = patientList.getValue();
         temporaryList.remove(patient);
-        pacienteList.setValue(temporaryList);
+        patientList.setValue(temporaryList);
     }
 
     public MutableLiveData<Patient> getActualPatient() {
@@ -54,14 +54,14 @@ public class PatientViewModel extends ViewModel {
     }
 
     public void modifyPatient(Patient patient){
-        List<Patient> temporaryList = pacienteList.getValue();
-        for (int i = 0; i < pacienteList.getValue().size(); i++)
+        List<Patient> temporaryList = patientList.getValue();
+        for (int i = 0; i < patientList.getValue().size(); i++)
         {
-            if (patient.getId() == pacienteList.getValue().get(i).getId()) {
+            if (patient.getId() == patientList.getValue().get(i).getId()) {
                 temporaryList.remove(i);
                 temporaryList.add(i, patient);
             }
         }
-        pacienteList.setValue(temporaryList);
+        patientList.setValue(temporaryList);
     }
 }
