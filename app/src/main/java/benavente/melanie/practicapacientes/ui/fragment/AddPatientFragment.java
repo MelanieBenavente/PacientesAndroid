e70ab38a1ff5ba33b9621bba0fba8c18bde05ed4
DESKTOP-MK268CN\Mel <lavienvert3@gmail.com>
2023-02-19 19:11:21 +0100

    added new function in add patient fragment

--- app/src/main/java/benavente/melanie/practicapacientes/ui/fragment/AddPatientFragment.java
+++ app/src/main/java/benavente/melanie/practicapacientes/ui/fragment/AddPatientFragment.java
@@ -4,23 +4,66 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.GridLayoutManager;
+
+import java.util.List;
 
 import benavente.melanie.practicapacientes.databinding.AddPatientFragmentBinding;
+import benavente.melanie.practicapacientes.domain.entity.Patient;
+import benavente.melanie.practicapacientes.ui.activity.MainActivity;
+import benavente.melanie.practicapacientes.ui.adapter.PatientsAdapter;
+import benavente.melanie.practicapacientes.ui.viewmodel.PatientViewModel;
 
 public class AddPatientFragment extends Fragment {
 
         private AddPatientFragmentBinding binding;
+
+        private PatientViewModel viewModel;
 
         @Nullable
         @Override
         public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
                 binding = AddPatientFragmentBinding.inflate(getLayoutInflater());
                 View view = binding.getRoot();
+                configureView();
                 return view;
         }
+
+        @Override
+        public void onResume() {
+                super.onResume();
+
+        binding.anadirButton.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View view) {
+                        //si el campo está vacío
+                        if (binding.editTextEdad.getText().toString().equals("")) {
+                                Toast.makeText(getContext(), "Debes introducir una edad", Toast.LENGTH_SHORT).show();
+                         //si no está vacío
+                        } else {
+                        viewModel.addPatient(new Patient(binding.editTextNombre.getText().toString(),
+                                Integer.valueOf(binding.editTextEdad.getText().toString()),
+                                binding.checkboxEstado.isChecked(),
+                                viewModel.getpatientList().getValue().size()+1));
+                                getActivity().onBackPressed();
+                        }
+
+                }
+
+        });
+        }
+        private void configureView() {
+                viewModel = ViewModelProviders.of(getActivity()).get(PatientViewModel.class);
+
+        }
+
 
 }
